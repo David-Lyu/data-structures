9f55680b70f1f5ac32d5d6a95a5e47c6a258231a
David <lyu.david@yahoo.com>
2020-07-10 12:09:21 -0700

    added algorith section to the readme and created folder for learning recursion

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # data-structures
-Data Structure lessons from Udemy
+Data Structure & algorith lessons from Udemy
+
+Data Structure: Lessons 1 - 6
+Algorith : Lessons 7 -
---
+++ lesson_7-recursion/lesson.js
@@ -0,0 +1,38 @@
+//Factorial
+
+function findFactorialRecursive(number) {
+    if (number === 2) return 2;
+    return number + findFactorialRecursive(number - 1)
+    
+}
+// time complexity O(n)
+
+function findFactorialIterative(number) {
+    let product = 1;
+    for(let i = 2; i <= number; i++) {
+        product = product * i
+    }
+    return product;
+}
+//time complexity O(n)
+
+//Fibonacci // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 ...
+
+function fiboncciaIterative(n) {
+    let arr = [0, 1]
+    for(let i = 2; i < n; i++) {
+        arr.push(arr[i-2] + arr[i-1])
+    }
+    return fibNum[n];
+}
+fiboncciaIterative(3)
+
+function fibonacciRecursive(n) {
+    if(n < 2) {
+        return name;
+    }
+    return fibonacciRecursive(n - 1) + fibonacciRecursive(n -2)
+}
+
+fibonacciRecursive(3)
+
